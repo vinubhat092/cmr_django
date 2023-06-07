e6bcd80b1550d779f930427728470625e6afb52f
Vinayak R Bhat <vinayakrbhat@Saarthi-GHF4LRR29Y.local>
2023-06-07 21:00:40 +0530

    till models

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class Crm1Config(AppConfig):
+class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'crm1'
+    name = 'accounts'
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
 # Create your models here.
+
+class Customer(models.Model):
+    name = models.CharField(max_length=200,null=True)
+    phone = models.CharField(max_length=200,null=True)
+    email = models.CharField(max_length=200,null=True)
+    date_created = models.DateTimeField(auto_now_add=True,null=True)
+
+    def __str__(self):
+        return self.name
+    
+class Tag(models.Model):
+    name = models.CharField(max_length=200,null=True)
+
+    def __str__(self):
+        return self.name
+    
+class Product(models.Model):
+    category = (
+        ('Indoor','Indoor'),
+        ('out door','out door')
+    )
+    name= models.CharField(max_length=200,null=True)
+    price = models.FloatField(max_length=200,null=True)
+    category = models.CharField(max_length=200,null=True,choices=category)
+    description = models.CharField(max_length=200,null=True ,blank=True)
+    date_created = models.DateTimeField(auto_now_add=True,null=True)
+    Tag = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    status = (
+        ('pending','pending'),
+        ('Out for delivery','Out for delivery'),
+        ('Delivered','Delivered'),
+    )
+    customer = models.ForeignKey(Customer , null=True , on_delete = models.SET_NULL)
+    product = models.ForeignKey(Product , null=True , on_delete = models.SET_NULL)
+    date_created = models.DateTimeField(auto_now_add=True,null=True)
+    status = models.CharField(max_length=200,null=True,choices=status)
