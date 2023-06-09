d9197f2e7f011dd05586f1b11757a85c6eedfd86
Vinayak R Bhat <vinayakrbhat@Saarthi-GHF4LRR29Y.local>
2023-06-09 10:55:00 +0530

    views added

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path ,include
 from accounts.views import home,products,customer,dashboard
 urlpatterns = [
-    path('', dashboard ),
+    path('', home ),
     path('products/', products ),
     path('customer/', customer ),
     path('dashboard/', dashboard ),
--- accounts/views.py
+++ accounts/views.py
@@ -3,7 +3,22 @@ from django.http import HttpResponse
 # Create your views here.
 from .models import *
 def home(request):
-    return render(request,"accounts/main.html")
+    print("comingnge")
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
+    customers_count = customers.count()
+    orders_count = orders.count()
+    orders_delivered = orders.filter(status="Delivered").count()
+    orders_pending = orders.filter(status="pending").count()
+    print("sf",customers)
+    context = {
+        "orders":orders,
+        "customers":customers,
+        "orders_count":orders_count,
+        "orders_delivered":orders_delivered,
+        "orders_pending":orders_pending,
+    }
+    return render(request,"accounts/dashboard.html",context)
 
 def dashboard(request):
     return render(request,"accounts/dashboard.html")
