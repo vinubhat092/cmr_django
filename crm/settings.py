efd7ef95b9472817fb6381c87fca158f0dbdde44
vinubhat092 <vinayakbhat94@gmail.com>
2023-07-08 17:30:36 +0530

    final commit

--- crm/settings.py
+++ crm/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'crm.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Demo_test',
+        'USER': 'postgres',
+        'PASSWORD': 'Vikki1928!@#',
+        'HOST': 'localhost',
+        'PORT':'5432'
     }
 }
 
